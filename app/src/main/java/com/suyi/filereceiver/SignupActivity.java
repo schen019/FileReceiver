0e1b0798507e40cf7efef9f992e66e067e18e963
schen019 <schen019@plattsburgh.edu>
2022-04-25 21:00:02 -0400

    0.3

--- app/src/main/java/com/suyi/filereceiver/SignupActivity.java
+++ app/src/main/java/com/suyi/filereceiver/SignupActivity.java
@@ -2,12 +2,14 @@ package com.suyi.filereceiver;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.ParseException;
 import com.parse.ParseUser;
