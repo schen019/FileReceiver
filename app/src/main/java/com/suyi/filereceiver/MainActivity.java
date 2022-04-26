8b7d0f54a2453d0f5d4019448efbb066f0bd5e48
schen019 <schen019@plattsburgh.edu>
2022-04-26 12:56:08 -0400

    0.3

--- app/src/main/java/com/suyi/filereceiver/MainActivity.java
+++ app/src/main/java/com/suyi/filereceiver/MainActivity.java
@@ -2,11 +2,16 @@ package com.suyi.filereceiver;
 
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    private TextView tvHello;
+    private BottomNavigationView btnNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
