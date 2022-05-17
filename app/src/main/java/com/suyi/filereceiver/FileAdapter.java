acbe2e91389b15e8880e55cecbd9cc80bfc8d28e
Henry <HDERR001@plattsburgh.edu>
2022-05-16 23:25:27 -0400

    download feature

--- app/src/main/java/com/suyi/filereceiver/FileAdapter.java
+++ app/src/main/java/com/suyi/filereceiver/FileAdapter.java
@@ -1,6 +1,10 @@
 package com.suyi.filereceiver;
 
+import static android.content.Context.DOWNLOAD_SERVICE;
+
+import android.app.DownloadManager;
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +13,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 
 import java.text.SimpleDateFormat;
@@ -57,6 +63,7 @@ public class FileAdapter extends RecyclerView.Adapter<FileAdapter.ViewHolder>
         ImageView ivImage;
         TextView tvReceiver;
         TextView tvDate;
+        Button btnDownload;
 
 
         public ViewHolder(@NonNull View itemView)
@@ -66,25 +73,49 @@ public class FileAdapter extends RecyclerView.Adapter<FileAdapter.ViewHolder>
             ivImage = itemView.findViewById(R.id.ivImage);
             tvReceiver = itemView.findViewById(R.id.tvReceiver);
             tvDate = itemView.findViewById(R.id.tvDate);
-
+            btnDownload = itemView.findViewById(R.id.btnDownload);
 
         }
 
 
-        public void bind(File file)
-        {
+        public void bind(File file) {
             tvSender.setText(file.getSender());
             tvReceiver.setText(file.getReceiver());
             tvDate.setText(file.getDate());
             ivImage.setImageResource(R.drawable.ic_baseline_file_copy_24);
 
+
             ParseFile fileImage = file.getFile();
-            if (fileImage != null)
-            {
+            if (fileImage != null) {
                 Glide.with(context).load(fileImage.getUrl()).into(ivImage);
             }
+            btnDownload.setOnClickListener(new View.OnClickListener() {
+
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onClick(View view) {
+
+                    downloadFile(fileImage);
+
+                }
+            });
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        private void downloadFile(ParseFile file) {
+            try {
+                String name = file.getFile().toString();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            DownloadManager downloadManager = (DownloadManager) context.getSystemService(DOWNLOAD_SERVICE);
+            downloadManager.addCompletedDownload(file.getName(), file.getName(), true, "text/plain","/sdcard/Android/data/com.suyi.my_parstagams/files/Pictures",10000,true);
+
+
 
         }
+
 
     }
 
